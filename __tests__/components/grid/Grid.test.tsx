ebef4563758a808e8fa50789acab2c7bf68737bd
gabrielszg <gabrielsouza5@hotmail.com>
2024-06-21 15:19:03 -0300

    test: click on the trash icon and delete the product

--- __tests__/components/grid/Grid.test.tsx
+++ __tests__/components/grid/Grid.test.tsx
@@ -9,7 +9,9 @@ import {render, cleanup, fireEvent} from '@testing-library/react-native';
 import Grid from '../../../src/components/Grid';
 import {Product} from '../../../src/models/product';
 
-jest.spyOn(Alert, 'alert');
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
 
 const mockSetProducts = jest.fn();
 
@@ -42,6 +44,22 @@ const products: Product[] = [
 
 describe('Grid Test', () => {
   afterEach(cleanup);
+
+  const mockHandleDelete = jest.fn().mockImplementation(() => {
+    const product = products[index];
+    products.splice(index, 1);
+    products.filter(item => item !== product);
+  });
+  const index = products.indexOf(products[3]);
+  const alertSpy = jest.spyOn(Alert, 'alert').mockImplementation(
+    (title, message, buttons) =>
+      //@ts-ignore
+      (buttons[1] = {
+        text: 'Ok',
+        onPress: () => mockHandleDelete(index),
+        style: 'default',
+      }),
+  );
 
   it('see if item is on the list', () => {
     const {getByText} = render(
@@ -85,5 +103,24 @@ describe('Grid Test', () => {
       'Deseja excluir este item?',
       expect.any(Array),
     );
+  });
+
+  it('when you click on the trash icon a dialog box opens and when you click ok delete the product from the list', () => {
+    const {getAllByTestId} = render(
+      <Grid products={products} setProducts={mockSetProducts} />,
+    );
+
+    const deleteButton = getAllByTestId('deleteButton');
+    fireEvent.press(deleteButton[3]);
+    expect(deleteButton.length).toEqual(4);
+
+    //@ts-ignore
+    expect(alertSpy.mock.calls[0][2][1].text).toEqual('Ok');
+
+    //@ts-ignore
+    alertSpy.mock.calls[0][2][1].onPress();
+
+    expect(mockHandleDelete).toHaveBeenCalledWith(3);
+    expect(products.length).toEqual(3);
   });
 });
